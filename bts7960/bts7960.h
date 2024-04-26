3a051d982828cabd3a00057a66dbe57a48005ac2
SteelPh0enix <wojciech_olech@hotmail.com>
2024-04-26 22:49:49 +0200

    Header refactor and more diagnostics

--- bts7960/bts7960.h
+++ bts7960/bts7960.h
@@ -22,12 +22,16 @@
 /// platform. HAL is the library's back-end, providing control over the actual hardware of the MCU.
 /// See `bts7960_hal.h` file for details.
 ///
-/// Most of the functions will return a BTS7960_Status value. Whether it's reasonable to check it
+/// All of the functions will return a BTS7960_Result value. Whether it's reasonable to check it
 /// depends on HAL implementation. For example, if setting the I/O pin state can never fail, the
-/// function will also never fail, so there's no reason to check the returned status.
-/// However, it's recommended to validate the return value of those functions whenever possible
-/// anyway, as compiler optimizations should detect and remove branches of code that can never be
-/// executed in final binary, making it a free "safety net" for the future.
+/// function will also never fail (except for very obvious reasons), so there's no reason to check
+/// the returned status if you're sure it won't fail due to software issue. However, it's
+/// recommended to validate the return value of those functions whenever possible anyway, as
+/// compiler optimizations should detect and remove branches of code that can never be executed in
+/// final binary, making it a free "safety net" for the future.
+///
+/// In **extreme** scenario, one may put some of those "obvious" and verbose checks behind a macro.
+/// But for now, this is good enough.
 
 #include <inttypes.h>
 #include <stdbool.h>
@@ -37,94 +41,128 @@
 
 /// BTS7960 instance.
 typedef struct BTS7960_t {
-  BTS7960_HAL *hal;     ///< A pointer to HAL instance.
-  bool is_initialized;  ///< Flag set by `Initialize` to indicate readiness.
+  BTS7960_HAL *hal;              ///< Pointer to a HAL instance.
+  uint16_t fault_voltage;        ///< Voltage on current sense pin when driver is in fault mode,
+                                 ///< in millivolts.
+  uint16_t current_sense_ratio;  ///< Current sense ratio.
+  bool is_initialized;           ///< Flag set by `Initialize` to indicate readiness.
 } BTS7960;
 
 /// BTS7960 state, returned by BTS7960_checkState() function.
 typedef struct BTS7960_State_t {
-  bool fault;        ///< If true, the driver is currently in fault mode.
   uint16_t current;  ///< Current flowing through the driver, in milliamperes.
-} BTS7960_State;
+  bool fault;        ///< If true, the driver is currently in fault mode.
+} BTS7960_Status;
 
-/// Enumeration representing a status of performed BTS7960 driver operation.
-typedef enum BTS7960_Status_t {
+/// Enumeration representing a result of performed operation.
+typedef enum BTS7960_Result_t {
   BTS7960_OK,                         ///< Operation was successful.
   BTS7960_HAL_ERROR,                  ///< HAL error, details should be saved in HAL instance.
+  BTS7960_ALREADY_INITIALIZED,        ///< HAL tried to be re-initialized.
+  BTS7960_NOT_INITIALIZED,            ///< HAL tried to be used before initialization.
   BTS7960_ERROR_INVALID_POWER_VALUE,  ///< Invalid value was passed to power-related function.
 #ifdef BTS7960_ENABLE_FREQUENCY_CONTROL
   BTS7960_ERROR_FREQUENCY_TOO_LOW,   ///< The frequency is too low for the hardware to generate.
   BTS7960_ERROR_FREQUENCY_TOO_HIGH,  ///< The frequency is too high for the hardware to generate.
 #endif
-  BTS7960_FAULT_DETECTED,  ///< A fault was detected during current measurements.
-} BTS7960_Status;
+  BTS7960_FAULT_DETECTED,  ///< A fault was detected during status check.
+} BTS7960_Result;
 
-/// Initializes BTS7960 structure and underlying hardware.
+
+/// Initializes the driver structure and underlying hardware.
 /// @important You should call this function first.
 /// @important Provided HAL instance should be initialized manually by the user
 ///            before passing it to this function.
 /// @param[in] bts Pointer to BTS7960 driver instance.
 /// @param[in] hal Pointer to user-provided HAL instance.
+/// @param[in] fault_voltage Voltage on current-sense pin in fault mode, in millivolts. The current
+///                          sense resistor value should guarantee that this voltage will not rise
+///                          above maximum voltage tolerated by ADC. You've been warned.
+/// @param[in] current_sense_ratio Current sense ratio, default is stored in
+///                                BTS7960_DEFAULT_CURRENT_SENSE_RATIO constant.
 /// @retval BTS7960_OK If initialization was successful.
-/// @retval BTS7960_HAL_ERROR If initialization has fails due to internal HAL error.
-BTS7960_Status BTS7960_initialize(BTS7960 *const bts, BTS7960_HAL *const hal);
+/// @retval BTS7960_ALREADY_INITIALIZED If the instance is already initialized.
+/// @retval BTS7960_HAL_ERROR If initialization failed due to an internal HAL error.
+BTS7960_Result BTS7960_initialize(BTS7960 *const bts, BTS7960_HAL *const hal,
+                                  const uint16_t fault_voltage, const uint16_t current_sense_ratio);
 
-/// De-initialize BTS7960, freeing the underlying hardware.
+/// De-initialize the driver, freeing the underlying hardware.
 /// @param[in] bts Pointer to BTS7960 driver instance.
 /// @retval BTS7960_OK If de-initialization was successful.
-/// @retval BTS7960_HAL_ERROR If de-initialization has fails due to internal HAL error.
-BTS7960_Status BTS7960_deInitialize(BTS7960 *const bts);
+/// @retval BTS7690_NOT_INITIALIZED If driver is not initialized.
+/// @retval BTS7960_HAL_ERROR If de-initialization failed due to an internal HAL error.
+BTS7960_Result BTS7960_deInitialize(BTS7960 *const bts);
 
-/// Enables the BTS7960 driver output.
-/// When enabled, you can use BTS7960_setPowerPercentage() set power percentage,
-/// or BTS7960_setRawPower() to set raw PWM value.
+/// Enables the driver's output.
 /// @param[in] bts Pointer to BTS7960 driver instance.
-/// @retval BTS7960_OK if enabling succeeded.
-/// @retval BTS7960_HAL_ERROR if enabling fails due to internal HAL error.
-BTS7960_Status BTS7960_enable(BTS7960 *const bts);
+/// @retval BTS7960_OK If enabling succeeded.
+/// @retval BTS7690_NOT_INITIALIZED If driver is not initialized.
+/// @retval BTS7960_HAL_ERROR If enabling failed due to an internal HAL error.
+BTS7960_Result BTS7960_enable(BTS7960 *const bts);
 
-/// Disables the BTS7960 driver output.
+/// Disables the driver's output.
 /// @param[in] bts Pointer to BTS7960 driver instance.
-/// @retval BTS7960_OK if disabling succeeded.
-/// @retval BTS7960_HAL_ERROR if disabling fails due to internal HAL error.
-BTS7960_Status BTS7960_disable(BTS7960 *const bts);
+/// @retval BTS7960_OK If disabling succeeded.
+/// @retval BTS7690_NOT_INITIALIZED If driver is not initialized.
+/// @retval BTS7960_HAL_ERROR If disabling failed due to an internal HAL error.
+BTS7960_Result BTS7960_disable(BTS7960 *const bts);
 
 /// Checks if the driver is currently enabled.
+/// Output parameters are cleared/set to 0 on error.
 /// @param[in] bts Pointer to BTS7960 driver instance.
-/// @retval true Driver is currently enabled.
-/// @retval false Driver is currently disabled.
-bool BTS7960_isEnabled(BTS7960 const *const bts);
+/// @param[out] status `true` if driver is enabled, `false` otherwise.
+/// @retval BTS7960_OK If getting the status succeeded.
+/// @retval BTS7690_NOT_INITIALIZED If driver is not initialized.
+/// @retval BTS7960_HAL_ERROR If getting the status failed due to an internal HAL error.
+BTS7960_Result BTS7960_isEnabled(BTS7960 const *const bts, bool *const status);
 
-/// Sets the power output of BTS7960 in 0-100% range (with integer precision).
+/// Measures the current flowing through BTS7960 driver and returns it (in milliamps).
+/// Also informs if a fault happened.
+/// Output parameters are set to 0 on error, except in BTS7960_FAULT_DETECTED case, where the
+/// current is set to 0 but the flag remains true.
 /// @param[in] bts Pointer to BTS7960 driver instance.
-/// @param[in] percent Percentage of power in 0-100 range.
-/// @retval BTS7960_OK if setting the power succeeded.
-/// @retval BTS7960_ERROR_INVALID_POWER_VALUE if `percent` is outside of [0, 100] range.
-/// @retval BTS7960_HAL_ERROR if setting the power fails due to internal HAL error.
-BTS7960_Status BTS7960_setPowerPercentage(BTS7960 *const bts, uint8_t const percent);
+/// @param[out] status Current status of the driver.
+/// @retval BTS7960_OK If getting the status succeeded.
+/// @retval BTS7960_FAULT_DETECTED If a driver's fault is detected.
+/// @retval BTS7690_NOT_INITIALIZED If driver is not initialized.
+/// @retval BTS7960_HAL_ERROR If getting the status failed due to an internal HAL error.
+BTS7960_Result BTS7960_getStatus(BTS7960 const *const bts, BTS7960_Status *const status);
 
-/// Gets current power output of BTS7960 in 0-100% range (with integer precision)
+/// Sets the power output of driver in 0-100% range.
 /// @param[in] bts Pointer to BTS7960 driver instance.
-/// @returns Current power output, in percentages.
-uint8_t BTS7960_getPowerPercentage(BTS7960 const *const bts);
+/// @param[in] percent Percentage of driver's power in 0-100 range.
+/// @retval BTS7960_OK If setting the power succeeded.
+/// @retval BTS7690_NOT_INITIALIZED If driver is not initialized.
+/// @retval BTS7960_ERROR_INVALID_POWER_VALUE If `percent` is outside of [0, 100] range.
+/// @retval BTS7960_HAL_ERROR If setting the power failed due to an internal HAL error.
+BTS7960_Result BTS7960_setPowerPercentage(BTS7960 *const bts, uint8_t const percent);
+
+/// Gets current power output of BTS7960 in 0-100[%] range (with integer precision).
+/// Output parameters are cleared/set to 0 on error.
+/// @param[in] bts Pointer to BTS7960 driver instance.
+/// @param[out] percentage Current power output in 0-100[%] range.
+/// @retval BTS7960_OK If getting the power succeeded.
+/// @retval BTS7690_NOT_INITIALIZED If driver is not initialized.
+/// @retval BTS7960_HAL_ERROR If getting the power failed due to an internal HAL error.
+BTS7960_Result BTS7960_getPowerPercentage(BTS7960 const *const bts, uint8_t *const percentage);
 
 #ifdef BTS7960_ENABLE_FREQUENCY_CONTROL
 /// Sets the output signal frequency.
 /// @param[in] bts Pointer to BTS7960 driver instance.
 /// @param[in] frequency Frequency of the signal, in hertz.
-/// @retval BTS7960_OK if setting the power succeeded.
-/// @retval BTS7960_ERROR_FREQUENCY_TOO_LOW if `frequency` is too low.
-/// @retval BTS7960_ERROR_FREQUENCY_TOO_HIGH if `frequency` is too high.
-/// @retval BTS7960_HAL_ERROR if setting the frequency fails due to internal HAL error.
-BTS7960_Status BTS7960_setOutputFrequency(BTS7960 *const bts, uint32_t const frequency);
+/// @retval BTS7960_OK If setting the frequency succeeded.
+/// @retval BTS7690_NOT_INITIALIZED If driver is not initialized.
+/// @retval BTS7960_ERROR_FREQUENCY_TOO_LOW If `frequency` is too low.
+/// @retval BTS7960_ERROR_FREQUENCY_TOO_HIGH If `frequency` is too high.
+/// @retval BTS7960_HAL_ERROR If setting the frequency failed due to an internal HAL error.
+BTS7960_Result BTS7960_setOutputFrequency(BTS7960 *const bts, uint32_t const frequency);
 
 /// Gets the current output signal frequency.
+/// Output parameters are cleared/set to 0 on error.
 /// @param[in] bts Pointer to BTS7960 driver instance.
-/// @returns Output signal frequency.
-uint32_t BTS7960_getOutputFrequency(BTS7960 const *const bts);
+/// @param[out] frequency Frequency of the signal, in hertz.
+/// @retval BTS7960_OK If getting the frequency succeeded.
+/// @retval BTS7690_NOT_INITIALIZED If driver is not initialized.
+/// @retval BTS7960_HAL_ERROR If getting the frequency failed due to an internal HAL error.
+BTS7960_Result BTS7960_getOutputFrequency(BTS7960 const *const bts, uint32_t *const frequency);
 #endif
-
-/// Measures the current flowing through BTS7960 driver and returns it (in milliamps).
-/// @param[in] bts Pointer to BTS7960 driver instance.
-/// @return BTS7960_Status structure with current driver status.
-BTS7960_Status BTS7960_getStatus(BTS7960 const *const bts);
--- bts7960/bts7960_hal.h
+++ bts7960/bts7960_hal.h
@@ -4,10 +4,10 @@
 /// BTS7690 hardware abstraction layer
 /// You have to provide an implementation of BTS_7960_HAL structure, and all the related functions
 /// yourself. You can either use a pre-existing implementation, or provide your own.
-/// Implementation can be selected via a macro.
-/// It's recommended to store informations about errors in HAL structure for error handling.
-/// HAL implementation may also provide additional functions, for example PWM frequency control,
-/// if available on target platform.
+/// Implementation must be linked manually.
+/// It's recommended to store informations about errors in HAL structure for error handling or
+/// debugging. HAL implementation may also provide additional functions, for example PWM frequency
+/// control, if available on target platform.
 /// @important **HAL instance must be initialized manually by the user before using BTS7960
 ///            driver!**
 /// @note When using high-frequency PWM as input: BTS7960 input signal frequency limit depends on
@@ -20,30 +20,68 @@
 /// Declaration of user-defined BTS7960 HAL structure.
 typedef struct BTS7960_HAL_impl BTS7960_HAL;
 
+/// Default current sense ratio.
+static const uint16_t BTS7960_DEFAULT_CURRENT_SENSE_RATIO = 8500;
+
 /// Initializes the hardware required for BTS7960 to operate.
 /// @param[in] hal Initialized BTS7960 HAL instance.
 /// @retval true The hardware was configured successfully.
 /// @retval false There was an error during hardware configuration.
-bool BTS7960_HAL_configureHardware(BTS7960_HAL *const hal);
+bool BTS7960_HAL_initializeHardware(BTS7960_HAL *const hal);
 
 /// De-initializes the hardware required for BTS7960 to operate.
-/// If de-initialization is not required, it may be left empty.
 /// @param[in] hal Initialized BTS7960 HAL instance.
 /// @retval true The hardware was de-initialized successfully.
 /// @retval false There was an error during hardware de-initialization.
-bool BTS7960_HAL_disableHardware(BTS7960_HAL *const hal);
+bool BTS7960_HAL_deInitializeHardware(BTS7960_HAL *const hal);
 
-/// Sets the PWM signal modulation percentage on driver's input.
+/// Sets driver's `enable` pin state.
 /// @param[in] hal Initialized BTS7960 HAL instance.
-/// @param[in] percentage PWM signal duty cycle percentage.
-bool BTS7960_HAL_setPwmSignalPercentage(BTS7960_HAL *const hal, uint8_t const percentage);
+/// @param[in] state State of the pin.
+/// @retval true The pin's state has been set.
+/// @retval false The pin's state couldn't be set.
+bool BTS7960_HAL_setEnablePinState(BTS7960_HAL *const hal, bool const state);
 
-/// Sets the PWM signal modulation frequency on driver's input.
+/// Gets driver's `enable` pin state.
 /// @param[in] hal Initialized BTS7960 HAL instance.
-/// @param[in] frequency Frequency of PWM signal.
-bool BTS7960_HAL_setPwmSignalFrequency(BTS7960_HAL *const hal, uint32_t const frequency);
+/// @param[out] state Current state of `enable` pin.
+/// @retval true State of the pin has been fetched.
+/// @retval false Couldn't fetch the state of the pin.
+bool BTS7960_HAL_getEnablePinState(BTS7960_HAL *const hal, bool *const state);
 
 /// Returns the voltage on current sense (IS) pin in millivolts.
 /// @param[in] hal Initialized BTS7960 HAL instance.
-/// @returns The voltage on current sense (IS) pin, in millivolts.
-uint16_t BTS7960_HAL_measureCurrentSenseVoltage(BTS7960_HAL *const hal);
+/// @param[out] voltage Voltage on the current sense pin, in millivolts.
+/// @retval true The voltage has been measured successfully.
+/// @retval false Couldn't measure the current sense voltage.
+bool BTS7960_HAL_measureCurrentSenseVoltage(BTS7960_HAL *const hal, uint16_t *const voltage);
+
+/// Sets the PWM signal duty cycle percentage.
+/// @param[in] hal Initialized BTS7960 HAL instance.
+/// @param[in] percentage PWM signal duty cycle percentage.
+/// @retval true PWM signal duty cycle has been set.
+/// @retval false Couldn't set the PWM signal duty cycle.
+bool BTS7960_HAL_setPwmSignalPercentage(BTS7960_HAL *const hal, uint8_t const percentage);
+
+/// Gets the PWM signal duty cycle percentage.
+/// @param[in] hal Initialized BTS7960 HAL instance.
+/// @param[out] percentage PWM signal duty cycle percentage.
+/// @retval true PWM signal duty cycle has been fetched.
+/// @retval false Couldn't fetch the PWM signal duty cycle.
+bool BTS7960_HAL_getPwmSignalPercentage(BTS7960_HAL *const hal, uint8_t *const percentage);
+
+#ifdef BTS7960_ENABLE_FREQUENCY_CONTROL
+/// Sets the PWM signal frequency.
+/// @param[in] hal Initialized BTS7960 HAL instance.
+/// @param[in] frequency Frequency of the PWM signal.
+/// @retval true PWM signal frequency has been set.
+/// @retval false Couldn't set the PWM signal frequency.
+bool BTS7960_HAL_setPwmSignalFrequency(BTS7960_HAL *const hal, uint32_t const frequency);
+
+/// Gets the PWM signal frequency.
+/// @param[in] hal Initialized BTS7960 HAL instance.
+/// @param[out] frequency Frequency of the PWM signal.
+/// @retval true PWM signal frequency has been fetched.
+/// @retval false Couldn't fetch the PWM signal frequency.
+bool BTS7960_HAL_getPwmSignalFrequency(BTS7960_HAL *const hal, uint32_t *const frequency);
+#endif
