5ccff9b07bbbce60b2ca3cec3a347ebd5e08cf0f
SteelPh0enix <wojciech_olech@hotmail.com>
2024-05-01 15:10:06 +0200

    Cleared the mock

--- bts7960/hal-impls/mock.c
+++ bts7960/hal-impls/mock.c
@@ -7,16 +7,3 @@
 
 struct BTS7960_HAL_impl {};
 
-bool BTS7960_HAL_configureHardware(BTS7960_HAL *const hal) { return false; }
-
-bool BTS7960_HAL_disableHardware(BTS7960_HAL *const hal) { return false; }
-
-bool BTS7960_HAL_setPwmSignalPercentage(BTS7960_HAL *const hal, uint8_t const percentage) {
-  return false;
-}
-
-bool BTS7960_HAL_setPwmSignalFrequency(BTS7960_HAL *const hal, uint32_t const frequency) {
-  return false;
-}
-
-uint16_t BTS7960_HAL_measureCurrentSenseVoltage(BTS7960_HAL *const hal) { return 0; }
